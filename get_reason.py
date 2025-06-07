a0cb4e32618f47f267d2513630d5e1fe80efe261
Eduarda Donato <eduardaribeiro0202@hotmail>
2025-06-07 08:55:51 -0300

    [feat]: start function get reason from atf number

--- get_reason.py
+++ get_reason.py
@@ -1,55 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-from config import PATH, EPROCESSO_URL, ATF_URL, USERNAME,PASSWORD
-from get_indeferidos import get_indeferidos
-
-processes = get_indeferidos(PATH)
+from config import ATF_URL, USERNAME_, PASSWORD
 
 driver = webdriver.Chrome()
-driver.get(EPROCESSO_URL)
+driver.get(ATF_URL)
 
-username_field = driver.find_element(By.NAME, "username")
-username_field.send_keys(USERNAME)
+WebDriverWait(driver, 30).until(
+    EC.frame_to_be_available_and_switch_to_it((By.ID, "contents"))
+)
 
-password_field = driver.find_element(By.NAME, "password")
-password_field.send_keys(PASSWORD)
+username = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.ID, "login")))
+username.send_keys(USERNAME_)
 
-login_button = driver.find_element(By.NAME, "submit")
+password = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.NAME, "edtDsSenha")))
+password.send_keys(PASSWORD)
+
+login_button = driver.find_element(By.NAME, "btnAvancar")
 login_button.click()
 
-time.sleep(10)
-
-menu_button = driver.find_element(By.ID, "menu-atf")
-menu_button.click()
-
-time.sleep(10)
-
-consultar_button = driver.find_element(By.ID, "58519")
-consultar_button.click()
-
-time.sleep(15)
-
-for process in processes:
-
-    try:
-        protocolo_field = driver.find_element(By.CSS_SELECTOR, 'input[formcontrolname="numeroProtocolo"]')
-        protocolo_field.clear()
-        protocolo_field.send_keys(process)
-        
-        consultar_button = driver.find_element(By.CSS_SELECTOR, "button.btn.btn-danger")
-        consultar_button.click()
-        
-        time.sleep(15)
-        
-        protocolo_atf = driver.find_elements(By.XPATH, '//span[@class="responsive-title"]')[2]
-        protocolo_atf.text
-        print(protocolo_atf)
-        
-    except Exception as e:
-        print(f"Erro no processo {process}: {e}")
-        continue
+time.sleep(30)
 driver.quit()
-
-
