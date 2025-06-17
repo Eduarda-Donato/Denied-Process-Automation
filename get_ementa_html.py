85950166f4cde191e2d3a378c475e86e2d1668e7
Eduarda Donato <meduardaribeiro0202@gmail.com>
2025-06-17 10:43:01 -0300

    [fix]: get ementa in html

--- get_ementa_html.py
+++ get_ementa_html.py
@@ -7,38 +7,36 @@ from config import HTML_PATH, PROCESS_ATF_PATH, JSON_EMENTA_PATH
 
 with open(PROCESS_ATF_PATH, 'r', encoding='utf-8') as f:
     dados = json.load(f)
+    
+selector = "p.MsoNormal, p.MsoBodyTextIndent2, p.MsoBlockText, p.MsoBodyText, .Default b, h1, h2, p"
 
-files = os.listdir(HTML_PATH)
 
 for obj in dados:
     atf = obj.get("atf")
-    ementa = ""
-
     if not atf:
         obj["ementa"] = ""
         continue
 
+    ementa = ""
 
-    for file in files:
-        if file.endswith('.html'):
-            html_path = os.path.join(HTML_PATH, file)
-            
-            with open(html_path, 'r', encoding='utf-8') as f:
-                html = f.read()
-                
-            soup = BeautifulSoup(html, "html.parser")
-            
-            for p in soup.select("p.MsoNormal"):
-                p_text = p.get_text()
-                if "INDEFERIMENTO" in p_text:
-                    ementa = p_text.strip()
-                    break
-            
-            if ementa:
+    fname = f"{atf}.html"
+    html_path = os.path.join(HTML_PATH, fname)
+
+    if os.path.exists(html_path):
+        with open(html_path, "r", encoding='utf-8') as f:
+            html = f.read()
+
+        soup = BeautifulSoup(html, "html.parser")
+
+        elements = soup.select(selector)
+        for el in elements:
+            text = el.get_text().strip()
+            if "INDEFERIMENTO" in text or "PEDIDO DENEGADO" in text:
+                ementa = text
                 break
             
     obj['ementa'] = ementa
     
-with open(JSON_EMENTA_PATH, 'w', encoding='utf-8') as f_out:
+with open("JSON_EMENTA_PATH", 'w', encoding='utf-8') as f_out:
     json.dump(dados, f_out, ensure_ascii=False, indent=4)
     
